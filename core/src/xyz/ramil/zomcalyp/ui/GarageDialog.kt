e6482fa2f68fefabc3a049b03118763e86e5ee78
ramilabdrakhmanov <ramilabdrakhmanov17@gmail.com>
2020-07-15 10:03:06 +0500

    add GarageDialog.kt

--- core/src/xyz/ramil/zomcalyp/ui/GarageDialog.kt
+++ core/src/xyz/ramil/zomcalyp/ui/GarageDialog.kt
@@ -2,14 +2,17 @@ package xyz.ramil.zomcalyp.ui
 
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.math.Vector2
+import com.badlogic.gdx.scenes.scene2d.Action
 import com.badlogic.gdx.scenes.scene2d.Actor
 import com.badlogic.gdx.scenes.scene2d.InputEvent
 import com.badlogic.gdx.scenes.scene2d.InputListener
+import com.badlogic.gdx.scenes.scene2d.actions.Actions
 import com.badlogic.gdx.scenes.scene2d.ui.*
+import com.badlogic.gdx.scenes.scene2d.utils.DragListener
 import com.badlogic.gdx.utils.Align
-import xyz.ramil.zomcalyp.ShopDialog
 import xyz.ramil.zomcalyp.ZGame
-import xyz.ramil.zomcalyp.entities.car.Car
+
 
 class GarageDialog internal constructor(var zgame: ZGame) : Dialog("", zgame.skin) {
 
@@ -24,19 +27,31 @@ class GarageDialog internal constructor(var zgame: ZGame) : Dialog("", zgame.ski
     val label2: Label
     val label:Label
 
+    val actionScaleOn:Action
+    val actionScaleOff:Action
+
+    var deltaCount = 0f
+    val rotationDirect = true
+
 
 
     init {
-
+        actionScaleOn = Actions.scaleBy(0f, 1f, 1f)
+        actionScaleOff = Actions.scaleBy(1f, 1f, 1f)
 
 
         val contentTable = contentTable
-//        contentTable.debug = true
+        contentTable.debug = true
         val image0 = Image(zgame.assetManager.get("cars/y0.png", Texture::class.java))
         val image1 = Image(zgame.assetManager.get("cars/y1.png", Texture::class.java))
         val image2 = Image(zgame.assetManager.get("cars/b0.png", Texture::class.java))
         val image3 = Image(zgame.assetManager.get("cars/b1.png", Texture::class.java))
         val image4 = Image(zgame.assetManager.get("cars/r0.png", Texture::class.java))
+
+
+
+
+
 
         carList.add(image0)
         carList.add(image1)
@@ -44,18 +59,29 @@ class GarageDialog internal constructor(var zgame: ZGame) : Dialog("", zgame.ski
         carList.add(image3)
         carList.add(image4)
 
+        for(actor in carList) {
+            actorRotation(actor)
+        }
+
+        Gdx.app.log("X"+image3.originX, "Y"+image3.originY)
+
+//        for(actor in carList) {
+//            rotation(actor)
+//            actor.setOrigin(Align.center)
+//        }
+
 
 
 
         val titleTable = Table()
-        label = Label("Super GT Turbo", zgame.skin)
+        label = Label("Super GT Turbo(Available from level 5)", zgame.skin)
         label.setAlignment(Align.center)
         titleTable.add(label).pad(10f)
         contentTable.add(titleTable)
         contentTable.row()
 
         val grandSelectTable = Table()
-//        grandSelectTable.debug = true
+        grandSelectTable.debug = true
 
         val tableLeft = Table()
         buttonleft = TextButton("<<", zgame.skin)
@@ -63,7 +89,7 @@ class GarageDialog internal constructor(var zgame: ZGame) : Dialog("", zgame.ski
         grandSelectTable.add(tableLeft).pad(20f)
 
         selectTable.add(carList.get(position))
-        grandSelectTable.add(selectTable)
+        grandSelectTable.add(selectTable).padLeft(30f).padRight(30f)
 
         val tableRight = Table()
         buttonRight = TextButton(">>", zgame.skin)
@@ -96,8 +122,24 @@ class GarageDialog internal constructor(var zgame: ZGame) : Dialog("", zgame.ski
         buttonSelect.setColor(buttonSelect.color.r, buttonSelect.color.g, buttonSelect.color.b, 0.7f)
         buttonCancel.setColor(buttonCancel.color.r, buttonCancel.color.g, buttonCancel.color.b, 0.7f)
 
+
+
         handleInputButton()
     }
+
+
+    private fun actorRotation(actor: Actor) {
+        actor.addListener(object : DragListener() {
+            override fun touchDragged(event: InputEvent?, x: Float, y: Float, pointer: Int) {
+                actor.setOrigin(actor.width/2, actor.height/2)
+                val touchDegree = Vector2(x, y).sub(Vector2(actor.getOriginX(), actor.getOriginY())).angle()
+                actor.rotateBy (touchDegree)
+                actor.debug = true
+                Gdx.app.log("X"+actor.originX, "Y"+actor.originY)
+            }
+        })
+    }
+
     private fun handleInputButton() {
         buttonleft.addListener(object : InputListener() {
             override fun touchUp(event: InputEvent, x: Float, y: Float, pointer: Int, button: Int) {
@@ -106,14 +148,17 @@ class GarageDialog internal constructor(var zgame: ZGame) : Dialog("", zgame.ski
                 if(position > 0) {
                     position--
                     label2.setText(""+(position+1)+"/"+(carList.size))
-                    selectTable.children[0] = carList[position]
+                    selectTable.children[0].remove()
+                    selectTable.add(carList[position])
+
                 } else {
                     position = carList.size-1;
                     label2.setText(""+(position+1)+"/"+(carList.size))
-                    selectTable.children[0] = carList[position]
+                    selectTable.children[0].remove()
+                    selectTable.add(carList[position])
+
                 }
             }
-
             override fun touchDown(event: InputEvent, x: Float, y: Float, pointer: Int, button: Int): Boolean {
                 buttonleft.setColor(buttonleft.color.r, buttonleft.color.g, buttonleft.color.b, 0.5f)
                 return true
@@ -127,11 +172,13 @@ class GarageDialog internal constructor(var zgame: ZGame) : Dialog("", zgame.ski
                 if(position < carList.size-1) {
                     position++
                     label2.setText(""+(position+1)+"/"+(carList.size))
-                    selectTable.children[0] = carList[position]
+                    selectTable.children[0].remove()
+                    selectTable.add(carList[position])
                 } else {
                     position = 0;
                     label2.setText(""+(position+1)+"/"+(carList.size))
-                    selectTable.children[0] = carList[position]
+                    selectTable.children[0].remove()
+                    selectTable.add(carList[position])
                 }
             }
 
